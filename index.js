449aefe2dd4a691729bbd4bc908cbd490904260c
tschumdev <tschumdev@gmail.com>
2018-08-23 16:58:21 -0400

    Change Connection

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8090;
 
 const http = require('http').Server(app);
 const io = require("socket.io")(http);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("."));
 
 io.on('connection', function(client) {
     console.log("Connection established");
