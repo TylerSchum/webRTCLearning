add2de7d2da4a1190cc3665e7090e53cda9103f3
tschumdev <tschumdev@gmail.com>
2018-08-24 12:04:14 -0400

    Change entire project to more mature style with Okta Login

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 const express = require("express");
 const app = express();
-const hostname = process.env.IP || "127.0.0.1";
 const port = process.env.PORT || 8090;
 
-const http = require('http').Server(app);
-const io = require("socket.io")(http);
-app.use(express.static("."));
+app.use(express.static("public"));
 
-io.on('connection', function(client) {
-    console.log("Connection established");
-    client.on("candidate", function(msg){
-        console.log("candidate message recieved!");
-        client.broadcast.emit("candidate", msg);
-    });
-    client.on("sdp", function(msg){
-        console.log("sdp message broadcasted!");
-        client.broadcast.emit("sdp", msg);
-    });
-    client.on("desc", function(desc){
-        console.log("description received!");
-        client.broadcast.emit("desc", desc);
-    });
-    client.on("answer", function(answer){
-        console.log("answer broadcasted");
-        client.broadcast.emit("answer", answer);
-    });
+app.use('/scripts', express.static(`${__dirname}/node_modules/`))
+
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
 });
-
-http.listen(port);
